00c2d86acbc3bd768f08506570a3b2dddec037ab
jiwon <gione4545@naver.com>
2026-04-13 16:41:55 +0900

    yam file remove secret

--- src/main/java/com/disaster/safety/dto/DetectionRequest.java
+++
@@ -1,9 +0,0 @@
-package com.disaster.safety.dto;
-
-import lombok.Data;
-
-@Data
-public class DetectionRequest {
-    private String cctvType;
-    private String imageBase64;
-}
---
+++ src/main/java/com/disaster/safety/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.disaster.safety.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByUsername(String username);
+    
+}
